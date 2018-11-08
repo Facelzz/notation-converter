4c5df469ad57b096cba9080a203a1f224769f3d4
Facelzz <44780931+Facelzz@users.noreply.github.com>
2018-11-08 11:33:21 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/**
+*	Индивидуальное задание
+*	по АВС
+**/
+
+#include <iostream>
+
+// bool checker(int, int);
+
+// int toDecimal(int, int)
+
+// void toBinary(int*, int, bool*)
+
+// bool *summ(bool*, bool*) or void add(bool*, bool*, bool*)
+
+// void printBinary(bool*);
+
+int main() {
+	int X = 0, Y = 0;
+	bool binariX[12], binaryY[12], binaryResult[12];
+	int _X = 0, _Y = 0;
+
+	return 0;
+}
