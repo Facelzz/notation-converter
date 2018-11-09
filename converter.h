23c74a7f6530479ebe0b58b7e9a3b0d55b8a9021
Facelzz <ilya.facelzz@gmail.com>
2018-11-10 01:22:36 +0300

    Rename "main.cpp" to "converter.cpp" Delete some unnecessary code Create a header file Write "isCorrect" function

---
+++ converter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+bool isCorrect(int, int);
+
+// int toDecimal(int, int)
+
+// void toBinary(int*, int, bool*)
+
+// bool *summ(bool*, bool*) or void add(bool*, bool*, bool*)
+
+// void printBinary(bool*);
