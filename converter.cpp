ba07c79ef7118fa6938148d7a3f3ca1ef7c8cd86
Facelzz <ilya.facelzz@gmail.com>
2018-11-11 23:33:09 +0300

    Write a "toBinary" function

--- converter.cpp
+++ converter.cpp
@@ -34,4 +34,26 @@ void printBinary(bool* binaryNumber) {
 	std::cout<<binaryNumber[0]<<".";
 	for(int i = 1; i < 12; i++)
 		std::cout<<binaryNumber[i];
+  std::cout<<std::endl;
+}
+
+void toBinary(int number, int numberSystem, bool* result)	{
+  for(int i = 0; i < 12; i++, result[i] = 0);
+  if(isCorrect(number, numberSystem))	{
+		if(numberSystem != 10)
+			number = toDecimal(number, numberSystem);
+		if(number < 0)  {
+			result[0] = 1;
+      number *= -1;
+    } else  {
+        result[0] = 0;
+      }
+		int i = 11;
+		while(number > 0)	{
+			result[i] = number % 2;
+			number /= 2;
+			i--;
+		}
+	}	else
+			std::cout<<"\n\tError! The number or number system are wrong!\n";
 }
--- converter.h
+++ converter.h
@@ -2,9 +2,9 @@
 
 bool isCorrect(int, int);
 
-int toDecimal(int, int)
+int toDecimal(int, int);
 
-// void toBinary(int*, int, bool*)
+void toBinary(int, int, bool*);
 
 // bool *summ(bool*, bool*) or void add(bool*, bool*, bool*)
 
